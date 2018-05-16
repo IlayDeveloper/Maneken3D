55137f05c5fd2e49647093eed7a29dc4d6b06b5e
IlayDeveloper <mr.tsyman@mail.ru>
2018-05-16 18:23:33 +0500

    Camera rotation

---
+++ Assets/Scripts/Camera/RotateController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateController : MonoBehaviour {
+
+	public int rotateSpeed = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void RotateX ()
+	{
+		 transform.RotateAround(Vector3.zero, Vector3.right, rotateSpeed * Time.deltaTime);
+	}
+
+	void RotateY ()
+	{
+		 transform.RotateAround(Vector3.zero, Vector3.up, rotateSpeed * Time.deltaTime);
+	}
+}
